cf1a49e54ee799a4f99c7f93564a4566d85fc428
SantiagoHV <sherrerav@correo.udistrital.edu.co>
2021-08-15 11:26:35 -0500

    take-assessment design ready

--- src/components/FormAssessment/FormAssessment.js
+++ src/components/FormAssessment/FormAssessment.js
@@ -1,9 +1,8 @@
-import React from "react";
-import {Form} from "react-bootstrap";
+import React, {useState} from "react";
+import {Col, Form, Row} from "react-bootstrap";
 import PrettoSlider from "../SliderCustom/SliderCustom";
 import './FormAssessment.css'
-import {Slider} from "@material-ui/core";
-
+import levels from "../../models/levels";
 
 const FormAssessment = (props) => {
 
@@ -13,7 +12,7 @@ const FormAssessment = (props) => {
             <div>
                 {props.categories.map((item, index) => {
                     return(
-                        <Form.Group key={index}>
+                        <Form.Group key={index} className={'form-group'}>
                             <Form.Label>{item.name}</Form.Label>
                             <PrettoSlider
                                 valueLabelDisplay="auto"
@@ -24,6 +23,7 @@ const FormAssessment = (props) => {
                                 max={6}
                                 onChange={(e, data) => {props.onChange(data,index)}}
                             />
+                            {/*<p>{levels.getLevel(item.value)}</p>*/}
                         </Form.Group>
                     )
                 })}
@@ -33,29 +33,42 @@ const FormAssessment = (props) => {
 
     return(
         <Form onSubmit={props.onSubmit} className={'form'}>
-            <Form.Group className="mb-3">
-                <Form.Label>Nombre Completo</Form.Label>
-                <Form.Control
-                    type={'text'}
-                    placeholder={'Ingresa tu nombre completo'}
-                    onChange={props.onChangeFormValue}
-                    name={'name'}
-                />
-            </Form.Group>
-            <Form.Group className="mb-3">
-                <Form.Label>Email</Form.Label>
-                <Form.Control
-                    type={'email'}
-                    placeholder={'Ingresa tu email'}
-                    onChange={props.onChangeFormValue}
-                    name={'email'}
-                />
-            </Form.Group>
-            {sliders()}
-            <div className={'space-btn'}>
-                <button className={'btn my-btn-primary form--btn'}>Enviar</button>
-            </div>
+            <Row>
+                <Col sm={12} md={6}
+                     className={'form__basic-data'}
+                >
+                    <h2>¡Aquí inicia tu camino!</h2>
+                    <div className={'basic-data__form-group'}>
+                        <Form.Group className="mb-3">
+                            <Form.Label>Nombre Completo</Form.Label>
+                            <Form.Control
+                                type={'text'}
+                                placeholder={'Ingresa tu nombre completo'}
+                                onChange={props.onChangeFormValue}
+                                name={'name'}
+                            />
+                        </Form.Group>
+                        <Form.Group className="mb-3">
+                            <Form.Label>Email</Form.Label>
+                            <Form.Control
+                                type={'email'}
+                                placeholder={'Ingresa tu email'}
+                                onChange={props.onChangeFormValue}
+                                name={'email'}
+                            />
+                        </Form.Group>
+                    </div>
+                </Col>
+                <Col sm={6} className={'center-container'}>
+                    <div className={'sliders-container'}>
+                        {sliders()}
+                        <div className={'space-btn'}>
+                            <button className={'btn my-btn-primary form--btn'}>Enviar</button>
+                        </div>
+                    </div>
 
+                </Col>
+            </Row>
         </Form>
     )
 }
--- src/pages/TakeAssestment/TakeAssessment.js
+++ src/pages/TakeAssestment/TakeAssessment.js
@@ -1,22 +1,37 @@
 import React, {useState} from "react";
-import {Container} from "react-bootstrap";
-
+import {Col, Container, Row} from "react-bootstrap";
+import GIRL from '../../assets/img/señalando.png'
 import FormAssessment from "../../components/FormAssessment/FormAssessment";
 import './TakeAssessment.css'
 
 const TakeAssessment = (props) => {
 
     return (
-        <section>
+        <section id={'take-assessment'}>
             <Container className={'form-container'}>
-                <FormAssessment
-                    categories={props.categories}
-                    name={props.basicData.name}
-                    email={props.basicData.email}
-                    onChangeFormValue={props.onChangeFormValue}
-                    onChange={props.onChangeSlider}
-                    onSubmit={props.onSubmit}
-                />
+                <Row className={'form-container-row'}>
+                    <Col sm={9}>
+                        <FormAssessment
+                            categories={props.categories}
+                            name={props.basicData.name}
+                            email={props.basicData.email}
+                            onChangeFormValue={props.onChangeFormValue}
+                            onChange={props.onChangeSlider}
+                            onSubmit={props.onSubmit}
+                        />
+                    </Col>
+                    <Col sm={3}>
+                        <div className={'img-container'}>
+                            <img
+                                src={GIRL}
+                                alt={'Chica señalando'}
+                                className={'take-assessment--img'}
+                            />
+                        </div>
+
+                    </Col>
+                </Row>
+
             </Container>
         </section>
     )
