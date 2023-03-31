5a0ee0ccec7d6c9ebacb30a1f953abe0610a7659
Santiago Herrera Velásquez <santiherreravelas@gmail.com>
2023-03-30 20:49:46 -0500

    commit small

--- src/api/config.js
+++ src/api/config.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const URL_API = process.env.API || 'http://localhost:8000';
+const URL_API = process.env.REACT_APP_API || 'http://localhost:8000';
+console.log(URL_API)
 
 const Axios = axios.create({
     baseURL: URL_API,
